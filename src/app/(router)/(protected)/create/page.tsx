35d37cc8503c0e51a085a6782e0a97115c73472b
YusufDodarov18 <email@github.com>
2026-03-12 22:25:36 +0500

    create post2

--- src/app/(router)/(protected)/create/page.tsx
+++ src/app/(router)/(protected)/create/page.tsx
@@ -3,12 +3,14 @@ import { Button } from "@/components/ui/button";
 import { Container } from "@mui/material";
 import React, { useState } from "react";
 import CreatePostModal from "./createPost";
+import { useTranslation } from "react-i18next";
 
 export default function page() {
   const [open, setOpen] = useState(false);
+  const { t } = useTranslation();
   return (
     <Container>
-      <Button variant={"outline"} onClick={() => setOpen(true)}>create</Button>
+      <Button variant={"outline"} onClick={() => setOpen(true)}>{t("layout.create")}</Button>
       <CreatePostModal open={open} onClose={() => setOpen(false)} />
     </Container>
   );
